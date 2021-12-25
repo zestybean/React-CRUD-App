bd372b76d3df569b1d1d4c25b5130154300fe613
Sam <samhalpern58@gmail.com>
2021-12-25 12:56:48 -0600

    postgresql docker script

---
+++ scripts/db_init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+
+# run with 'SKIP_DOCKER=<bool> ./db_init.sh'
+
+set -x
+set -eo pipefail
+
+# set your db vars
+DB_USER=${POSTGRES_USER:=postgres}
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_NAME="${POSTGRES_DB:=newsletter}"
+DB_PORT="${POSTGRES_PORT:=5432}"
+
+# Allow to skip Docker if a dockerized Postgres database is already running
+if [[ "$SKIP_DOCKER" = "false" ]];
+then
+docker run \
+	-e POSTGRES_USER=${DB_USER} \
+	-e POSTGRES_PASSWORD=${DB_PASSWORD} \
+	-e POSTGRES_DB=${DB_NAME} \
+	-p "${DB_PORT}":5432 \
+	-d postgres \
+	postgres -N 1000
+fi
+export PGPASSWORD="${DB_PASSWORD}"
+
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+	>&2 echo "Postgres is still unavailable - sleeping" 
+	sleep 1 
+done
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
+
+# copy and paste this line to your active shell session
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+cargo sqlx database create
+cargo sqlx migrate run
+
+>&2 echo "migrated and ready to party"
