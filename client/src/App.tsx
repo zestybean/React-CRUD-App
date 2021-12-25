26bd6e51d5d0742f53dcdea18455933dfd5c35cb
zestybean <brando.lugo@gmail.com>
2021-12-25 12:06:43 -0600

    Added a form to frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Hello Pastita!
+      <div className="title">
+        <h1>Hello Pastita!</h1>
+      </div>
+
+      <br />
+      <div className="clientForm">
+        <label htmlFor="">Name:</label>
+        <input type="text" />
+
+        <label htmlFor="">Age:</label>
+        <input type="number" />
+
+        <label htmlFor="">Position:</label>
+        <input type="text" />
+
+        <label htmlFor="">Wage(Year):</label>
+        <input type="number" />
+
+        <button>Add Employee</button>
+      </div>
     </div>
   );
 }
