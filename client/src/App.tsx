b16153a0194572e123673017faea132c882779d3
zestybean <brando.lugo@gmail.com>
2021-12-25 21:26:50 -0600

    File name changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Hello from './pages/Hello';
-import Index from './pages/Index';
-import Nav from './components/Nav';
+import Hello from "./pages/Hello/Hello";
+import Index from "./pages/Index/Home";
+import Nav from "./components/Nav/Nav";
 
 function App() {
   return (
     <Router>
       <Nav />
       <Routes>
-        <Route path='/' element={<Index />} />
-        <Route path='hello' element={<Hello />} />
+        <Route path="/" element={<Index />} />
+        <Route path="hello" element={<Hello />} />
       </Routes>
     </Router>
   );
