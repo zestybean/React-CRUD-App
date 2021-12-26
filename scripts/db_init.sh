504330bd9f706389a89933706584d768f7ae58a5
Sam <samhalpern58@gmail.com>
2021-12-25 21:13:29 -0600

    fixed db_init script

--- scripts/db_init.sh
+++ scripts/db_init.sh
@@ -12,8 +12,6 @@ DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
 # Allow to skip Docker if a dockerized Postgres database is already running
-if [[ "$SKIP_DOCKER" = "false" ]];
-then
 docker run \
 	-e POSTGRES_USER=${DB_USER} \
 	-e POSTGRES_PASSWORD=${DB_PASSWORD} \
@@ -21,14 +19,13 @@ docker run \
 	-p "${DB_PORT}":5432 \
 	-d postgres \
 	postgres -N 1000
-fi
 export PGPASSWORD="${DB_PASSWORD}"
 
-until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
-	>&2 echo "Postgres is still unavailable - sleeping" 
-	sleep 1 
-done
->&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
+# until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+# 	>&2 echo "Postgres is still unavailable - sleeping" 
+# 	sleep 1 
+# done
+# >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
 # copy and paste this line to your active shell session
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
