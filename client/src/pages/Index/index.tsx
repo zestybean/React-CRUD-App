bd6732b5cdab48560800533792861c4faa9c0769
Sam <samhalpern58@gmail.com>
2021-12-25 20:47:10 -0600

    server

---
+++ client/src/pages/Index/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function HomeForm() {
+  return(
+  <div className="App">
+      <div className="title">
+        <h1>Hello Pastita!</h1>
+      </div>
+
+      <br />
+      <div className="clientForm">
+        <label htmlFor="">Name:</label>
+        <input type="text" />
+
+        <label htmlFor="">Age:</label>
+        <input type="number" />
+
+        <label htmlFor="">Position:</label>
+        <input type="text" />
+
+        <label htmlFor="">Wage(Year):</label>
+        <input type="number" />
+
+        <button>Add Employee</button>
+      </div>
+    </div>
+  );
+}
+
+export default HomeForm;
